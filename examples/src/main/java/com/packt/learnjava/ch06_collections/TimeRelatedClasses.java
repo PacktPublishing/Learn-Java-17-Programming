796f38141acb467ba9fef7d50f4a6adc5e75f923
Nick Samoylov <nsamoylov@hotmail.com>
2021-12-24 10:05:51 -0700

    Better readability

--- examples/src/main/java/com/packt/learnjava/ch06_collections/TimeRelatedClasses.java
+++ examples/src/main/java/com/packt/learnjava/ch06_collections/TimeRelatedClasses.java
@@ -16,61 +16,68 @@ public class TimeRelatedClasses {
         localTime();
         localDateTime();
         periodAndDuration();
+        periodOfDay();
     }
 
     private static void localDate(){
-        System.out.println("\nlocalDate():");
+        System.out.println("\nlocalDate(): current date");
+        System.out.println(LocalDate.now());
+                     //prints: current date in format yyyy-MM-dd
 
-        System.out.println(LocalDate.now()); //prints: 2019-03-04
-
+        System.out.println("\nlocalDate(): all zoneIds");
         Set<String> zoneIds = ZoneId.getAvailableZoneIds();
         for(String zoneId: zoneIds){
             System.out.println(zoneId);
         }
 
+        System.out.println("\nlocalDate(): current date in Tokyo");
         ZoneId zoneId = ZoneId.of("Asia/Tokyo");
-        System.out.println(LocalDate.now(zoneId));   //prints: 2019-03-05
+        System.out.println(LocalDate.now(zoneId));
+             //prints: current date in Tokyo in format yyyy-MM-dd
 
-        LocalDate lc1 =  LocalDate.parse("2020-02-23");
-        System.out.println(lc1);                     //prints: 2020-02-23
+        System.out.println("\nlocalDate(): date 2023-02-23");
+        LocalDate lc1 =  LocalDate.parse("2023-02-23");
+        System.out.println(lc1);                     //prints: 2023-02-23
 
-        LocalDate lc2 =  LocalDate.parse("20200223", DateTimeFormatter.BASIC_ISO_DATE);
-        System.out.println(lc2);                     //prints: 2020-02-23
+        LocalDate lc2 =  LocalDate.parse("20230223", DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(lc2);                     //prints: 2023-02-23
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate lc3 =  LocalDate.parse("23/02/2020", formatter);
-        System.out.println(lc3);                     //prints: 2020-02-23
+        LocalDate lc3 =  LocalDate.parse("23/02/2023", formatter);
+        System.out.println(lc3);                     //prints: 2023-02-23
 
-        LocalDate lc4 =  LocalDate.of(2020, 2, 23);
-        System.out.println(lc4);                     //prints: 2020-02-23
+        LocalDate lc4 =  LocalDate.of(2023, 2, 23);
+        System.out.println(lc4);                     //prints: 2023-02-23
 
-        LocalDate lc5 =  LocalDate.of(2020, Month.FEBRUARY, 23);
-        System.out.println(lc5);                     //prints: 2020-02-23
+        LocalDate lc5 =  LocalDate.of(2023, Month.FEBRUARY, 23);
+        System.out.println(lc5);                     //prints: 2023-02-23
 
-        LocalDate lc6 = LocalDate.ofYearDay(2020, 54);
-        System.out.println(lc6);                     //prints: 2020-02-23
+        LocalDate lc6 = LocalDate.ofYearDay(2023, 54);
+        System.out.println(lc6);                     //prints: 2023-02-23
 
-        System.out.println(lc6.getYear());        //prints: 2020
+        System.out.println(lc6.getYear());        //prints: 2023
         System.out.println(lc6.getMonth());       //prints: FEBRUARY
         System.out.println(lc6.getMonthValue());  //prints: 2
         System.out.println(lc6.getDayOfMonth());  //prints: 23
-        System.out.println(lc6.getDayOfWeek());   //prints: SUNDAY
-        System.out.println(lc6.isLeapYear());     //prints: true
-        System.out.println(lc6.lengthOfMonth());  //prints: 29
-        System.out.println(lc6.lengthOfYear());   //prints: 366
+        System.out.println(lc6.getDayOfWeek());   //prints: THURSDAY
+        System.out.println(lc6.isLeapYear());     //prints: false
+        System.out.println(lc6.lengthOfMonth());  //prints: 28
+        System.out.println(lc6.lengthOfYear());   //prints: 365
 
-        System.out.println(lc6.withYear(2021));     //prints: 2021-02-23
-        System.out.println(lc6.withMonth(5));       //prints: 2020-05-23
-        System.out.println(lc6.withDayOfMonth(5));  //prints: 2020-02-05
-        System.out.println(lc6.withDayOfYear(53));  //prints: 2020-02-22
-        System.out.println(lc6.plusDays(10));       //prints: 2020-03-04
+        System.out.println("\nlocalDate(): date 2023-02-23 modified");
+        System.out.println(lc6.withYear(2024));     //prints: 2024-02-23
+        System.out.println(lc6.withMonth(5));       //prints: 2023-05-23
+        System.out.println(lc6.withDayOfMonth(5));  //prints: 2023-02-05
+        System.out.println(lc6.withDayOfYear(53));  //prints: 2023-02-22
+        System.out.println(lc6.plusDays(10));       //prints: 2020-03-05
         System.out.println(lc6.plusMonths(2));      //prints: 2020-04-23
-        System.out.println(lc6.plusYears(2));       //prints: 2022-02-23
-        System.out.println(lc6.minusDays(10));      //prints: 2020-02-13
-        System.out.println(lc6.minusMonths(2));     //prints: 2019-12-23
-        System.out.println(lc6.minusYears(2));      //prints: 2018-02-23
+        System.out.println(lc6.plusYears(2));       //prints: 2025-02-23
+        System.out.println(lc6.minusDays(10));      //prints: 2023-02-13
+        System.out.println(lc6.minusMonths(2));     //prints: 2022-12-23
+        System.out.println(lc6.minusYears(2));      //prints: 2021-02-23
 
-        LocalDate lc7 =  LocalDate.parse("2020-02-22");
+        System.out.println("\nlocalDate(): compare 2023-02-23 and 2023-02-22");
+        LocalDate lc7 =  LocalDate.parse("2023-02-22");
         System.out.println(lc6.isAfter(lc7));       //prints: true
         System.out.println(lc6.isBefore(lc7));      //prints: false
     }
@@ -78,34 +85,34 @@ public class TimeRelatedClasses {
     private static void localTime(){
         System.out.println("\nlocalTime():");
 
-        System.out.println(LocalTime.now());         //prints: 21:15:46.360904
+        System.out.println(LocalTime.now());        //prints: 21:15:46.360904
 
         ZoneId zoneId = ZoneId.of("Asia/Tokyo");
-        System.out.println(LocalTime.now(zoneId));   //prints: 12:15:46.364378
+        System.out.println(LocalTime.now(zoneId));  //prints: 12:15:46.364378
 
         LocalTime lt1 =  LocalTime.parse("20:23:12");
-        System.out.println(lt1);                     //prints: 20:23:12
+        System.out.println(lt1);                    //prints: 20:23:12
 
         LocalTime lt2 =  LocalTime.of(20, 23, 12);
-        System.out.println(lt2);                     //prints: 20:23:12
+        System.out.println(lt2);                    //prints: 20:23:12
 
-        System.out.println(lt2.getHour());           //prints: 20
-        System.out.println(lt2.getMinute());         //prints: 23
-        System.out.println(lt2.getSecond());         //prints: 12
-        System.out.println(lt2.getNano());           //prints: 0
+        System.out.println(lt2.getHour());          //prints: 20
+        System.out.println(lt2.getMinute());        //prints: 23
+        System.out.println(lt2.getSecond());        //prints: 12
+        System.out.println(lt2.getNano());          //prints: 0
 
-        System.out.println(lt2.withHour(3));         //prints: 03:23:12
-        System.out.println(lt2.withMinute(10));      //prints: 20:10:12
-        System.out.println(lt2.withSecond(15));      //prints: 20:23:15
-        System.out.println(lt2.withNano(300));       //prints: 20:23:12.000000300
-        System.out.println(lt2.plusHours(10));       //prints: 06:23:12
-        System.out.println(lt2.plusMinutes(2));      //prints: 20:25:12
-        System.out.println(lt2.plusSeconds(2));      //prints: 20:23:14
-        System.out.println(lt2.plusNanos(200));      //prints: 20:23:12.000000200
-        System.out.println(lt2.minusHours(10));      //prints: 10:23:12
-        System.out.println(lt2.minusMinutes(2));     //prints: 20:21:12
-        System.out.println(lt2.minusSeconds(2));     //prints: 20:23:10
-        System.out.println(lt2.minusNanos(200));     //prints: 20:23:11.999999800
+        System.out.println(lt2.withHour(3));        //prints: 03:23:12
+        System.out.println(lt2.withMinute(10));     //prints: 20:10:12
+        System.out.println(lt2.withSecond(15));     //prints: 20:23:15
+        System.out.println(lt2.withNano(300));      //prints: 20:23:12.000000300
+        System.out.println(lt2.plusHours(10));      //prints: 06:23:12
+        System.out.println(lt2.plusMinutes(2));     //prints: 20:25:12
+        System.out.println(lt2.plusSeconds(2));     //prints: 20:23:14
+        System.out.println(lt2.plusNanos(200));     //prints: 20:23:12.000000200
+        System.out.println(lt2.minusHours(10));     //prints: 10:23:12
+        System.out.println(lt2.minusMinutes(2));    //prints: 20:21:12
+        System.out.println(lt2.minusSeconds(2));    //prints: 20:23:10
+        System.out.println(lt2.minusNanos(200));    //prints: 20:23:11.999999800
 
         LocalTime lt4 =  LocalTime.parse("20:25:12");
         System.out.println(lt2.isAfter(lt4));       //prints: false
@@ -140,11 +147,11 @@ public class TimeRelatedClasses {
     private static void periodAndDuration() {
         System.out.println("\nperiodAndDuration():");
 
-        LocalDateTime ldt1 = LocalDateTime.parse("2020-02-23T20:23:12");
+        LocalDateTime ldt1 = LocalDateTime.parse("2023-02-23T20:23:12");
         LocalDateTime ldt2 = ldt1.plus(Period.ofYears(2));
-        System.out.println(ldt2);      //prints: 2022-02-23T20:23:12
+        System.out.println(ldt2);      //prints: 2025-02-23T20:23:12
 
-        LocalDateTime ldt = LocalDateTime.parse("2020-02-23T20:23:12");
+        LocalDateTime ldt = LocalDateTime.parse("2023-02-23T20:23:12");
         ldt.minus(Period.ofYears(2));
         ldt.plus(Period.ofMonths(2));
         ldt.minus(Period.ofMonths(2));
@@ -159,8 +166,8 @@ public class TimeRelatedClasses {
         ldt.plus(Duration.ofMillis(2));
         ldt.minus(Duration.ofMillis(2));
 
-        LocalDate ld1 =  LocalDate.parse("2020-02-23");
-        LocalDate ld2 =  LocalDate.parse("2020-03-25");
+        LocalDate ld1 =  LocalDate.parse("2023-02-23");
+        LocalDate ld2 =  LocalDate.parse("2023-03-25");
         Period period = Period.between(ld1, ld2);
         System.out.println(period.getDays());       //prints: 2
         System.out.println(period.getMonths());     //prints: 1
@@ -181,4 +188,49 @@ public class TimeRelatedClasses {
         System.out.println(duration.getNano());    //prints: 0
     }
 
+    private static void periodOfDay() {
+        System.out.println("\ndayPeriod():");
+
+        System.out.print("1: ");
+        periodOfDayFromDateTime("2023-03-23T05:05:18.123456", "MM-dd-yyyy h a");
+                                                                  //prints: 03-23-2023 5 AM
+
+        System.out.print("\n2: ");
+        periodOfDayFromDateTime("2023-03-23T05:05:18.123456", "MM-dd-yyyy h B");
+                                                             //prints: 03-23-2023 5 at night
+
+        System.out.print("\n3: ");
+        periodOfDayFromDateTime("2023-03-23T06:05:18.123456", "h B");
+                                                                   //prints: 6 in the morning
+
+        System.out.print("\n4: ");
+        periodOfDayFromTime("11:05:18.123456", "h B"); //prints: 11 in the morning
+
+        System.out.print("\n5: ");
+        periodOfDayFromTime("12:05:18.123456", "h B"); //prints: 12 in the afternoon
+
+        System.out.print("\n6: ");
+        periodOfDayFromTime("17:05:18.123456", "h B"); //prints: 5 in the afternoon
+
+        System.out.print("\n7: ");
+        periodOfDayFromTime("18:05:18.123456", "h B"); //prints: 6 in the evening
+
+        System.out.print("\n8: ");
+        periodOfDayFromTime("20:05:18.123456", "h B"); //prints: 8 in the evening
+
+        System.out.print("\n9: ");
+        periodOfDayFromTime("21:05:18.123456", "h B"); //prints: 9 at night
+    }
+
+    private static void periodOfDayFromDateTime(String time, String pattern){
+        LocalDateTime date = LocalDateTime.parse(time);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        System.out.print(date.format(formatter));
+    }
+
+    private static void periodOfDayFromTime(String time, String pattern){
+        LocalTime date = LocalTime.parse(time);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        System.out.print(date.format(formatter));
+    }
 }
